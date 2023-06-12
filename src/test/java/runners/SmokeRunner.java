bbfa877c106b4f28a95f01d85922bd62fccf0aa1
Samcodelover <samal010590@gmail.com>
2023-06-12 15:45:18 -0400

    test commit from subMain clone

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -7,13 +7,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         //features we use to provide the path of all the feature files
+        //test
         features = "src/test/resources/features/",
         glue = "steps",
         //when you set dryRun to true, it stops actual execution
         //it will quickly scan all the gherkin steps whether they are implemented or not
         //when we dryRun = false, it starts execute again
         dryRun = false,
-        tags = "@api",
+        tags = "@regression",
         //to remove irrelevant info from console, you need to monochrome to true
         monochrome = true,
         //pretty keyword prints the steps in the console to increase readability
