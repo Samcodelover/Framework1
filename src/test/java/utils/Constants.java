650cc3e4148ba27316464ce4540e05568eec7159
Samcodelover <samal010590@gmail.com>
2023-06-12 19:49:48 -0400

    Lala added something

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,6 +2,8 @@ package utils;
 
 public class Constants {
 
+    //Test
+
     public static final String CONFIGURATION_FILEPATH =
             System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
     public static final int EXPLICIT_WAIT = 20;
